58d894b1b7d18a01af4368d2534d9a4c42c3afc0
owaise <awais.muhammad.jans@gmail.com>
2024-06-08 17:23:24 +0500

    Implemented Firebase Push Notification with History

--- app/src/main/java/com/push/notification/saver/app/activities/FirstScreen.kt
+++ app/src/main/java/com/push/notification/saver/app/activities/FirstScreen.kt
@@ -1,28 +1,21 @@
 package com.push.notification.saver.app.activities
 
-import android.content.BroadcastReceiver
-import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
-import android.os.Build
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.localbroadcastmanager.content.LocalBroadcastManager
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.messaging.FirebaseMessaging
-import com.push.notification.saver.app.R
 import com.push.notification.saver.app.databinding.ActivityFirstScreenBinding
 import com.push.notification.saver.app.db.NotificationDatabaseHelper
-import com.push.notification.saver.app.services.AppConfig
+import java.util.Timer
+import java.util.TimerTask
 
-//import com.push.notification.saver.app.services.MyForegroundService
+
 
 
 class FirstScreen : AppCompatActivity() {
 
     private lateinit var b: ActivityFirstScreenBinding
+
+    private lateinit var timer: Timer
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,34 +23,29 @@ class FirstScreen : AppCompatActivity() {
         b = ActivityFirstScreenBinding.inflate(layoutInflater)
         setContentView(b.root)
 
-
         val db = NotificationDatabaseHelper(this)
-
         b.btnNotificationHistory.setOnClickListener {
             startActivity(Intent(this, NotificationHistoryScreen::class.java))
         }
 
-        b.idNumber.text = db.getAllNotifications().size.toString()
 
 
-        if (intent.extras != null) {
-            for (key in intent.extras!!.keySet()) {
 
-                if (key == AppConfig.NAME) {
-                    b.tvTitle.text = intent.extras!!.getString(key)
-                } else if (key == AppConfig.BODY) {
-                    b.tvMsg.text = intent.extras!!.getString(key)
-                } else {
-                    // do nothing
+
+        timer = Timer()
+        timer.schedule(object : TimerTask() {
+            override fun run() {
+                runOnUiThread {
+                    b.idNumber.text = db.getAllNotifications().size.toString()
+
                 }
             }
-        }
+        }, 0, 1000) // Update every 1000 milliseconds (1 second)
+
 
 
 
 
     }
-
-
 
 }
